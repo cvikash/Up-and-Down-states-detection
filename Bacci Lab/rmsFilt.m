3c5a40b019445dec0126e1c822f8c17aeef6c87a
cvikash <vikash7991vikki@gmail.com>
2019-08-28 16:58:17 +0200

    Delete rmsFilt.m

--- Bacci Lab/rmsFilt.m
+++
@@ -1,6 +0,0 @@
-function rmsFiltData = rmsFilt(data,win)
-nSamples = length(data);
-rmsFiltData = zeros(nSamples,1);
-for ii = 1:((floor(nSamples/win)-1)*win)
-    rmsFiltData(ii) = rms(data((1:win)+(ii-1)));    
-end
